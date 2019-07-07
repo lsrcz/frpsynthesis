93444e731ea08179d716b32db85a06f83699d6cd
Julie Newcomb <newcombj@cs.washington.edu>
2019-07-07 16:08:58 -0700

    more rx operators in discrete time reduction

--- rxjsdsl/discretetime/dsl.rkt
+++ rxjsdsl/discretetime/dsl.rkt
@@ -11,6 +11,48 @@
 ;; Note that some operators have the same names as Racket methods,
 ;; so we always add the prefix rx
 
+(define (getStatefulStream f init stream)
+  (for/list ([i (range 1 (add1 (length stream)))])
+    (foldl f init (take stream i))))
+
+;; http://reactivex.io/documentation/operators/combinelatest.html
+(define (rxCombineLatest f inputStream1 inputStream2)
+  (let ([stateStream1 (getStatefulStream (λ (evt accum)
+                                 (if (empty-event? evt)
+                                     accum
+                                     evt)) NOEVENT inputStream1)]
+        [stateStream2 (getStatefulStream (λ (evt accum)
+                                 (if (empty-event? evt)
+                                     accum
+                                     evt)) NOEVENT inputStream2)])
+    (for/list ([input-event1 inputStream1]
+               [input-event2 inputStream2]
+               [state-event1 stateStream1]
+               [state-event2 stateStream2])
+      (if (and (or (not-empty-event? input-event1) (not-empty-event? input-event2))
+               (not-empty-event? state-event1) (not-empty-event? state-event2))
+          (f state-event1 state-event2)
+          NOEVENT))))
+
+;; http://reactivex.io/documentation/operators/distinct.html
+(define (rxDistinct inputStream)
+  (let ([stateStream (getStatefulStream (λ (evt accum)
+                                 (if (empty-event? evt)
+                                     accum
+                                     evt)) NOEVENT inputStream)])
+    (cons (first inputStream)
+          (for/list ([input-event (cdr inputStream)]
+                     [previous-state stateStream])
+            (if (not (equal? input-event previous-state))
+                input-event
+                NOEVENT)))))
+
+;; http://reactivex.io/documentation/operators/filter.html
+(define (rxFilter pred inputStream)
+  (map (λ (evt) (if (or (empty-event? evt) (not (pred evt)))
+                    NOEVENT
+                    evt)) inputStream))
+
 ;; http://reactivex.io/documentation/operators/map.html
 (define (rxMap f inputStream)
   (for/list ([elt inputStream])
@@ -26,10 +68,39 @@
 
 ;; http://reactivex.io/documentation/operators/scan.html
 (define (rxScan f inputStream)
-  (for/list ([idx (range (length inputStream))])
-    (let ([evts (filter not-empty-event? (take inputStream (add1 idx)))])
-      (if (empty-event? (list-ref inputStream idx))
+  (let ([accumStream (getStatefulStream (λ (evt accum)
+                                (if (empty-event? evt)
+                                    accum
+                                    (if (empty-event? accum)
+                                        evt
+                                        (f accum evt)))) NOEVENT inputStream)])
+    (for/list ([input-event inputStream]
+               [accum-event accumStream])
+      (if (empty-event? input-event)
           NOEVENT
-          (if (equal? 1 (length evts))
-              (list-ref inputStream idx)
-              (foldl f (first evts) (cdr evts)))))))
+          accum-event))))
+
+;; http://reactivex.io/documentation/operators/skip.html
+(define (rxSkip n inputStream)
+  (let ([eventCounter (getStatefulStream (λ (evt accum)
+                                 (if (empty-event? evt)
+                                     accum
+                                     (add1 accum))) 0 inputStream)])
+    (for/list ([input-event inputStream]
+               [counter-event eventCounter])
+      (if (<= counter-event n)
+          NOEVENT
+          input-event))))
+
+
+;; http://reactivex.io/documentation/operators/take.html
+(define (rxTake n inputStream)
+  (let ([eventCounter (getStatefulStream (λ (evt accum)
+                                 (if (empty-event? evt)
+                                     accum
+                                     (add1 accum))) 0 inputStream)])
+    (for/list ([input-event inputStream]
+               [counter-event eventCounter])
+      (if (> counter-event n)
+          NOEVENT
+          input-event))))
--- rxjsdsl/discretetime/test-dsl.rkt
+++ rxjsdsl/discretetime/test-dsl.rkt
@@ -5,6 +5,14 @@
 (require "model.rkt")
 (require "dsl.rkt")
 
+(check-equal? (rxCombineLatest + (list 1 NOEVENT 2 NOEVENT 4 NOEVENT NOEVENT)
+                                 (list NOEVENT 1 NOEVENT NOEVENT NOEVENT 2 3))
+              (list NOEVENT 2 3 NOEVENT 5 6 7))
+
+(check-equal? (rxDistinct (list NOEVENT 1 1 NOEVENT 2 NOEVENT 2)) (list NOEVENT 1 NOEVENT NOEVENT 2 NOEVENT NOEVENT))
+
+(check-equal? (rxFilter even? (list 0 2 NOEVENT 3 4 NOEVENT)) (list 0 2 NOEVENT NOEVENT 4 NOEVENT))
+
 (check-equal? (rxMap add1 (list 0 1 NOEVENT 2)) (list 1 2 NOEVENT 3))
 
 (check-equal? (rxMerge (list 0 NOEVENT 2 NOEVENT) (list NOEVENT 1 NOEVENT 3)) (list 0 1 2 3))
@@ -14,3 +22,7 @@
 (check-equal? (rxScan + '()) '())
 (check-equal? (rxScan + (list NOEVENT 1)) (list NOEVENT 1))
 (check-equal? (rxScan + (list NOEVENT 1 NOEVENT 1 NOEVENT 2)) (list NOEVENT 1 NOEVENT 2 NOEVENT 4))
+
+(check-equal? (rxSkip 3 (list NOEVENT 1 NOEVENT 1 1 NOEVENT 3 2)) (list NOEVENT NOEVENT NOEVENT NOEVENT NOEVENT NOEVENT 3 2))
+
+(check-equal? (rxTake 3 (list NOEVENT 1 NOEVENT 1 1 NOEVENT 3 2)) (list NOEVENT 1 NOEVENT 1 1 NOEVENT NOEVENT NOEVENT))
